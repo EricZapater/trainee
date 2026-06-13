968d8f4bededd78901c7e9cac9ae5b8a317a1fbc
Èric Zapater <ericzm@ext.inditex.com>
2026-06-13 12:49:30 +0200

    Fix superpowers of admin

--- backend/internal/middleware/role.go
+++ backend/internal/middleware/role.go
@@ -9,7 +9,19 @@ import (
 func RequireRole(role string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		userRole, exists := c.Get("rol")
-		if !exists || userRole.(string) != role {
+		if !exists {
+			c.JSON(http.StatusForbidden, gin.H{"error": "no tens permisos per accedir a aquest recurs"})
+			c.Abort()
+			return
+		}
+
+		uRoleStr := userRole.(string)
+		if uRoleStr == "admin" {
+			c.Next()
+			return
+		}
+
+		if uRoleStr != role {
 			c.JSON(http.StatusForbidden, gin.H{"error": "no tens permisos per accedir a aquest recurs"})
 			c.Abort()
 			return
